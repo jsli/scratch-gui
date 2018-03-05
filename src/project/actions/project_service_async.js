bc7f0fd142cb3b14194c81613691abdb81cefc33
Manson Li <lijinsong@rouchi.com>
2018-03-05 11:40:52 +0800

    增加项目接口
    Signed-off-by: Manson Li <lijinsong@rouchi.com>

--- src/project/actions/project_service_async.js
+++ src/project/actions/project_service_async.js
@@ -2,7 +2,8 @@ import api from '../../api/apis.js';
 import {checkResponse} from '../../api/gateway';
 import {authHeader} from '../../api/auth';
 
-const saveOnlineProject = project => {
+// 保存项目-在线
+const saveOnlineProject = (project, sourceCode) => {
     const auth = authHeader();
     const requestOptions = {
         method: 'POST',
@@ -11,7 +12,70 @@ const saveOnlineProject = project => {
             'Authorization': `Basic ${auth}`
         },
         body: JSON.stringify({
-            source_code: project
+            source_code: sourceCode
+        })
+    };
+
+    return fetch(`${api.project}/${project.no}`, requestOptions)
+        .then(checkResponse)
+        .then(
+            data => {
+                // TODO: 保存项目成功
+                console.warn(data);
+            }
+        )
+        .catch(
+            errorResponse => {
+                errorResponse.json().then(
+                    error => {
+                        // TODO: 保存项目失败
+                        console.error(error);
+                    }
+                );
+            }
+        );
+};
+
+// 保存项目-本地
+const saveOfflineProject = (sourceCode, projectName = '') => {
+    // Download project data into a file - create link element,
+    // simulate click on it, and then remove it.
+    const saveLink = document.createElement('a');
+    document.body.appendChild(saveLink);
+
+    const data = new Blob([sourceCode], {type: 'text'});
+    const url = window.URL.createObjectURL(data);
+    saveLink.href = url;
+
+    if (projectName === '') {
+        // File name: project-DATE-TIME
+        const date = new Date();
+        const timestamp = `${date.toLocaleDateString()}-${date.toLocaleTimeString()}`;
+        projectName = `project-${timestamp}.json`;
+    }
+
+    saveLink.download = projectName;
+    saveLink.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(saveLink);
+};
+
+// 新建项目-在线
+const newOnlineProject = project => {
+    // todo: 刷新playgroud，是否需要放在这里？
+
+    const auth = authHeader();
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${auth}`
+        },
+        body: JSON.stringify({
+            name: project.name,
+            source_code: project.source_code,
+            version: 3,
+            memo: project.memo
         })
     };
 
@@ -33,30 +97,14 @@ const saveOnlineProject = project => {
         );
 };
 
-const saveOfflineProject = (project, projectName = '') => {
-    // Download project data into a file - create link element,
-    // simulate click on it, and then remove it.
-    const saveLink = document.createElement('a');
-    document.body.appendChild(saveLink);
-
-    const data = new Blob([project], {type: 'text'});
-    const url = window.URL.createObjectURL(data);
-    saveLink.href = url;
-
-    if (projectName === '') {
-        // File name: project-DATE-TIME
-        const date = new Date();
-        const timestamp = `${date.toLocaleDateString()}-${date.toLocaleTimeString()}`;
-        projectName = `project-${timestamp}.json`;
-    }
-
-    saveLink.download = projectName;
-    saveLink.click();
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(saveLink);
+// 新建项目-本地
+const newOfflineProject = project => {
+    // todo: 刷新playgroud，是否需要放在这里？
 };
 
 export const projectService = {
     saveOnlineProject,
-    saveOfflineProject
+    saveOfflineProject,
+    newOfflineProject,
+    newOnlineProject
 };
